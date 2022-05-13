e6339d486ab1d1f4384ca28a1283c9b941ae1781
Qinghuan <1041220793@qq.com>
2022-05-13 14:25:16 +0800

    feat: scrollbar styles

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,6 +3,7 @@ import Layout from './Layout.vue'
 import NotFound from './NotFound.vue'
 import DefaultTheme from 'vitepress/theme'
 import './index.css'
+import './styles/scroll.scss'
 export default {
   ...DefaultTheme,
   Layout,
